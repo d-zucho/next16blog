9187978edc9462584ce88b3470bde78673c674f0
d-zucho <danny.nico.dev@outlook.com>
2025-12-07 04:05:47 -0800

    initial commit + start header

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import Navbar from "@/components/web/Navbar";
+
+export default function Home() {
+  return (
+    <Navbar />
+  );
+}
---
+++ components/web/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
+
+      
+const Navbar = () => {
+  return (
+    <nav className='w-full py-5 flex items-center justify-between'>
+      <div className='flex items-center gap-8'>
+        <Link href="/">
+          <h1 className='text-3xl font-bold'>Next<span className='text-blue-500'>Pro</span></h1>
+        </Link>
+
+        <div className='flex items-center gap-2'>
+          <Link href="/">Home</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/create">Create</Link>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-2'>
+        <Link 
+          href="/auth/sign-up"
+          className={buttonVariants({ variant: "default" })}
+        >Sign up</Link>
+        <Link 
+          href="/auth/login"
+          className={buttonVariants({ variant: "secondary" })}
+        >Login</Link>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
